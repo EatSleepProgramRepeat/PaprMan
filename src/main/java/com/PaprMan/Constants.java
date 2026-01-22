022c34d90a1dd61f081335c50715bd0b6dc66ea9
Alexander McLean <mcleanalexander37@gmail.com>
2026-01-22 16:09:22 -0500

    Linux fixes

--- src/main/java/com/PaprMan/Constants.java
+++ src/main/java/com/PaprMan/Constants.java
@@ -2,6 +2,7 @@ package com.PaprMan;
 
 public class Constants {
     public static final String VERSION_NUMBER = "v1.0-ALPHA";
+    public static final String OS_NAME = System.getProperty("os.name");
 
     public static final int DEFAULT_STAGE_HEIGHT = 600;
     public static final int DEFAULT_STAGE_WIDTH = 800;
