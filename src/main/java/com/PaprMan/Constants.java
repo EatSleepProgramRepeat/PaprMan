9b7c5d1b51e7ac2b98775b2c3a81ad4f002600ae
Alexander McLean <mcleanalexander37@gmail.com>
2026-01-23 09:45:50 -0500

    Updated stage width to look better

--- src/main/java/com/PaprMan/Constants.java
+++ src/main/java/com/PaprMan/Constants.java
@@ -5,8 +5,8 @@ public class Constants {
     public static final String OS_NAME = System.getProperty("os.name");
     public static final boolean IS_LINUX = Constants.OS_NAME.matches(".*(nix|nux).*");
 
-    public static final int DEFAULT_STAGE_HEIGHT = 600;
-    public static final int DEFAULT_STAGE_WIDTH = 800;
+    public static final int DEFAULT_STAGE_HEIGHT = 400;
+    public static final int DEFAULT_STAGE_WIDTH = 570;
     public static final int THUMBNAIL_WIDTH = 160;
     public static final int THUMBNAIL_HEIGHT = 90;
 }
