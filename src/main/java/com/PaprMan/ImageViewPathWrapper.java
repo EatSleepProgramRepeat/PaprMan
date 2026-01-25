27be67377fa26597bedbb547e9b8b9d0e5d3b6b1
Alexander McLean <mcleanalexander37@gmail.com>
2026-01-24 21:17:02 -0500

    Made ImageViewPathWrapper

---
+++ src/main/java/com/PaprMan/ImageViewPathWrapper.java
@@ -0,0 +1,34 @@
+package com.PaprMan;
+
+import javafx.scene.image.ImageView;
+
+import java.nio.file.Path;
+
+public class ImageViewPathWrapper {
+    private ImageView imageView;
+    private Path path;
+
+    public ImageViewPathWrapper(
+            ImageView imageView,
+            Path path
+    ) {
+        this.imageView = imageView;
+        this.path = path;
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public void setImageView(ImageView imageView) {
+        this.imageView = imageView;
+    }
+
+    public Path getPath() {
+        return path;
+    }
+
+    public void setPath(Path path) {
+        this.path = path;
+    }
+}
